f407631d7f83658a99758ff79d51c4c1233c1c7c
beckinfonet <bakytbek.tatibekov@gmail.com>
2026-04-20 21:16:43 -0700

    Initial Commit

---
+++ validationLedger/validationLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  validationLedgerApp.swift
+//  validationLedger
+//
+//  Created by Beck Maldin VL on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct validationLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
